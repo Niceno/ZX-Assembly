9509d08a253ff16625f0de5d27a68bbcb2a60ff2
Niceno <bojan.niceno.scientist@gmail.com>
2025-08-27 20:21:05 +0200

    This is really cool: prints numbers right-aligned
    On branch main
    
    modified:   006_print_at.asm

--- Sources/006_print_at.asm
+++ Sources/006_print_at.asm
@@ -89,7 +89,44 @@ Main:
   ld (ycoord), a  ; set initial x coordinate.
   call SetCoords  ; set up our x/y coords.
 
-  ld bc, 12345
+  ; Check if it has 5 digits
+  ld  hl, (number)        ; store number in HL
+  ld  de, 10000           ; store dividend in DE
+  or a                    ; clear the c flag
+  sbc hl, de              ; HL = HL - DE
+  jr nc, NoMorePadding    ; HL > 10000
+  ld a, '_'               ; print an underscore
+  rst ROM_PRINT_A_1       ; display it
+
+  ; Check if it has 4 digits
+  ld  hl, (number)        ; store number in HL
+  ld  de, 1000            ; store dividend in DE
+  or a                    ; clear the c flag
+  sbc hl, de              ; HL = HL - DE
+  jr nc, NoMorePadding    ; HL > 1000
+  ld a, '_'               ; print an underscore
+  rst ROM_PRINT_A_1       ; display it
+
+  ; Check if it has 3 digits
+  ld  hl, (number)        ; store number in HL
+  ld  de, 100             ; store dividend in DE
+  or a                    ; clear the c flag
+  sbc hl, de              ; HL = HL - DE
+  jr nc, NoMorePadding    ; HL > 1000
+  ld a, '_'               ; print an underscore
+  rst ROM_PRINT_A_1       ; display it
+
+  ; Check if it has 2 digits
+  ld  hl, (number)        ; store number in HL
+  ld  de, 10              ; store dividend in DE
+  or a                    ; clear the c flag
+  sbc hl, de              ; HL = HL - DE
+  jr nc, NoMorePadding    ; HL > 1000
+  ld a, '_'               ; print an underscore
+  rst ROM_PRINT_A_1       ; display it
+
+NoMorePadding:
+  ld bc, (number)
   call ROM_STACK_BC  ; transform the number in BC register to floating point
   call ROM_PRINT_FP  ; print the floating point number in the calculator stack
 
@@ -113,14 +150,17 @@ SetCoords:
 ;
 ;%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 xcoord:
-  defb 0
+  defb 0                 ; defb = define byte
 
 ycoord:
-  defb 15
+  defb 15                ; defb = define byte
 
 bojanstring:
-  defb "Bojan is cool!"
+  defb "Bojan is cool!"  ; defb = define byte
 bojanstringend equ $
+
+number:
+  defw 10000             ; defw = define word  <---=
 
 ;-------------------------------------------------------------------------------
 ; Save a snapshot that starts execution at the address marked with Main
