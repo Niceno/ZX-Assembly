085fec32b6c2bc721be2fefe435347be265f8bea
Niceno <bojan.niceno.scientist@gmail.com>
2025-08-29 10:39:15 +0200

    VERY COOL: Wrote a sub to color a tex box!
    It has a double loop, the first I every wrote in assembly!
    
    On branch main
    
    modified:   006_print_at.asm

--- Sources/006_print_at.asm
+++ Sources/006_print_at.asm
@@ -92,7 +92,9 @@ Main:
 
   call Color_Text_Row
 
+  ;---------------
   ; Make a column
+  ;---------------
   ld A,  8
   ld (text_row), A      ; store row
   ld A, 24
@@ -101,6 +103,22 @@ Main:
   ld (text_color), A    ; store color
 
   call Color_Text_Column
+
+  ;------------
+  ; Make a box
+  ;------------
+  ld A, 10
+  ld (text_row), A      ; store row
+  ld A, 26
+  ld (text_column), A   ; store column coordinate
+  ld A, 3
+  ld (text_length), A   ; store box length
+  ld A, 3
+  ld (text_height), A   ; store box height
+  ld A, WHITE_INK + BLUE_PAPER
+  ld (text_color), A    ; store color
+
+  call Color_Text_Box
 
   ret  ; end of the main program
 
@@ -181,6 +199,77 @@ Set_Text_Coords:
  rst ROM_PRINT_A_1      ; print it.
 
  ret  ; end of subroutine
+
+;===============================================================================
+; Color_Tex_Box
+;-------------------------------------------------------------------------------
+; Purpose:
+; - Colors a box of text with specified length and height
+;
+; Parameters (passed via memory locations):
+; - text_row
+; - text_column
+; - text_length
+; - text_height
+;-------------------------------------------------------------------------------
+Color_Text_Box:
+
+  ;------------------------------------------------------------------------
+  ; Set initial value of HL to point to the beginning of screen attributes
+  ;------------------------------------------------------------------------
+  ld HL, MEM_SCREEN_COLORS  ; load HL with the address of screen color
+
+  ;------------------------------------------------------------------
+  ; Increase HL text_column times, to shift it to the desired column
+  ;------------------------------------------------------------------
+  ld A, (text_column)  ; prepare B as loop counter
+  ld B, A              ; ld B, (text_column) wouldn't work
+LoopColumns:
+  inc HL               ; increase HL text_row times
+  djnz LoopColumns     ; decrease B and jump if nonzero
+
+  ;-----------------------------------------------------------------
+  ; Increase HL text_row * 32 times, to shift it to the desired row
+  ;-----------------------------------------------------------------
+  ld A, (text_row)  ; prepare B as loop counter
+  ld B, A           ; ld B, (text_column) wouldn't work
+  ld DE, 32         ; there are 32 columns, this is not space character
+LoopRows:
+  add HL, DE        ; increase HL by 32
+  djnz LoopRows     ; decrease B and repeat the loop if nonzero
+
+  ;---------------------------------------------------------------
+  ; Now the HL holds the correct position of the screen attribute
+  ; perfrom a double loop throug rows and columns to color a box
+  ;---------------------------------------------------------------
+  ld A, (text_color)   ; prepare the color
+  ld C, A              ; store the color in C
+  ld A, (text_height)  ; A stores height, will be the outer loop
+
+LoopHeight:  ; loop over A, outer loop
+
+  ; Store HL at the first row position
+  push HL
+
+  ; Set (and re-set) B to text length, inner counter
+  ; You have to preserve (push/pop) in order to keep the outer counter value
+  push AF              ; A stores text height, store it before using A to fill B
+  ld A, (text_length)  ; prepare B as loop counter
+  ld B, A              ; B stores the lengt, will be inner loop
+  pop AF               ; restore A
+
+LoopLength:          ; loop over B, inner loop
+  ld (HL), C         ; set the color at position pointed by HL registers
+  inc HL             ; go to the next horizontal position
+  djnz LoopLength
+
+  pop HL             ; retreive the first positin in the row
+  add HL, DE         ; go to the next row
+
+  dec A              ; decrease A, outer loop counter
+  jr nz, LoopHeight  ; repeat if A is nonzero
+
+  ret  ; end of subroutine
 
 ;===============================================================================
 ; Color_Tex_Column
