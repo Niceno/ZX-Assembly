1904ef79f46c6e828d92fa73a03e383c1426aa33
Niceno <bojan.niceno.scientist@gmail.com>
2025-08-30 10:54:37 +0200

    Introduced function "Unpress"
    It waits until all the keys are unpressed.
    
    On branch main
    
    modified:   008_define_keys.asm

--- Sources/008_define_keys.asm
+++ Sources/008_define_keys.asm
@@ -410,26 +410,7 @@ DoneSetting:
   ;--------------------------------------
   ; Loop until all the keys are released
   ;--------------------------------------
-Unpress:
-
-  ; Set the HL to point to the beginning of array all_key_ports
-  ld HL, all_key_ports
-
-  ld D, 8  ; there are eight rows of keys
-BrowseKeyRows:
-
-  ; Load the port number into BC indirectly through HL
-  ld C, (HL)      ; low byte into C
-  inc HL
-  ld B, (HL)      ; high byte into B
-  inc HL
-  in A, (C)       ; read key states (1 = not pressed, 0 = pressed)
-  and  %00011111  ; mask out upper three bits, keep lower five
-  cp   %00011111  ; compare with all ones in lower five bits
-  jr nz, Unpress  ; if not all are 1, go back an read the keyboar again
-
-  dec D
-  jr nz, BrowseKeyRows
+  call Unpress
 
   ;--------------------------
   ; Retreive the key counter
@@ -444,6 +425,45 @@ BrowseKeyRows:
   rst ROM_PRINT_A_1     ; display it
 
   ret  ; end of the main program
+
+;===============================================================================
+; Unpress
+;-------------------------------------------------------------------------------
+; Purpose:
+; - Waits until all keys are unpressed
+;
+; Parameters (passed via memory locations)
+; - all_key_ports
+;-------------------------------------------------------------------------------
+Unpress:
+
+  ;-------------------------------------------------------------
+  ; Set the HL to point to the beginning of array all_key_ports
+  ;-------------------------------------------------------------
+  ld HL, all_key_ports
+
+  ;------------------------------
+  ; There are eight rows of keys
+  ;------------------------------
+  ld D, 8              ; there are eight rows of keys
+
+BrowseKeyRows:
+
+  ; Load the port number into BC indirectly through HL
+  ld C, (HL)      ; low byte into C
+  inc HL
+  ld B, (HL)      ; high byte into B
+  inc HL
+  in A, (C)       ; read key states (1 = not pressed, 0 = pressed)
+  and  %00011111  ; mask out upper three bits, keep lower five
+  cp   %00011111  ; compare with all ones in lower five bits
+  jr nz, Unpress  ; if not all are 1, go back an read the keyboar again
+
+  dec D
+
+  jr nz, BrowseKeyRows
+
+  ret  ; end of the subroutine
 
 ;===============================================================================
 ; Set_Text_Coords
