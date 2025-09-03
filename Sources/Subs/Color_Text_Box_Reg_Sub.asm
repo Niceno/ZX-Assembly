a3a831ed530ab3565eb3c5875694bf7ada5681b4
Niceno <bojan.niceno.scientist@gmail.com>
2025-09-03 08:57:36 +0200

    VERY IMPORTANT: Fixed a bug here!
    On branch main
    
    modified:   Subs/Color_Text_Box_Reg_Sub.asm

--- Sources/Subs/Color_Text_Box_Reg_Sub.asm
+++ Sources/Subs/Color_Text_Box_Reg_Sub.asm
@@ -29,9 +29,9 @@ Color_Text_Box_Reg_Sub:
   ; Increase HL text_column times, to shift it to the desired column
   ;------------------------------------------------------------------
   ld A, C
-  cp 0                                    ; is A equal to zero?
+  cp 0                                        ; is A equal to zero?
   jr z, Color_Text_Box_Reg_Skip_Zero_Columns  ; if so, skip this loop
-  ld B, C                                 ; use B as the counter
+  ld B, C                                     ; use B as the counter
 Color_Text_Box_Reg_Loop_Columns:
   inc HL                                ; increase HL text_row times
   djnz Color_Text_Box_Reg_Loop_Columns  ; decrease B and jump if nonzero
@@ -41,12 +41,12 @@ Color_Text_Box_Reg_Skip_Zero_Columns:   ; get here if column count is zero
   ;-----------------------------------------------------------------
   ; Increase HL text_row * 32 times, to shift it to the desired row
   ;-----------------------------------------------------------------
-  ld DE, 32                            ; 32 columns, this is not a space!!!
-  ld A, B                              ; fetch the row
-  cp 0                                 ; is A equal to zero?
+  ld DE, 32                                ; 32 columns, this is not a space!!!
+  ld A, B                                  ; fetch the row
+  cp 0                                     ; is A equal to zero?
   jr z, Color_Text_Box_Reg_Skip_Zero_Rows  ; if so, skip this loop
 Color_Text_Box_Reg_Loop_Rows:
-  add HL, DE                     ; increase HL by 32
+  add HL, DE                         ; increase HL by 32
   djnz Color_Text_Box_Reg_Loop_Rows  ; decrease B and repeat the loop if nonzero
 Color_Text_Box_Reg_Skip_Zero_Rows:   ; get here if row count is zero
 
@@ -61,20 +61,19 @@ Color_Text_Box_Reg_Skip_Zero_Rows:   ; get here if row count is zero
 
 Color_Text_Box_Reg_Loop_Height:  ; loop over A, outer loop
 
-  ; Store HL at the first row position
-  push HL
+  push DE  ; store DE which holds length (D) and height
+  push HL  ; store HL at the first row position
 
-  ; Set B (now counter) to text length, inner counter
-  ld B, D              ; B stores the lengt, will be inner loop
-
-  ld DE, 32
+  ld B, D    ; B stores the lengt, will be inner loop
+  ld DE, 32  ; number of columns now, to be added to HL
 Color_Text_Box_Reg_Loop_Length:  ; loop over B, inner loop
   ld (HL), C                     ; set color at position pointed by HL registers
   inc HL                         ; go to the next horizontal position
   djnz Color_Text_Box_Reg_Loop_Length
 
-  pop HL             ; retreive the first positin in the row
-  add HL, DE         ; go to the next row
+  pop HL      ; retreive the first positin in the row
+  add HL, DE  ; add the number of columns -> go to the next row
+  pop DE      ; retreive length (D) and height (E)
 
   dec A                                  ; decrease A, outer loop counter
   jr nz, Color_Text_Box_Reg_Loop_Height  ; repeat if A is nonzero
