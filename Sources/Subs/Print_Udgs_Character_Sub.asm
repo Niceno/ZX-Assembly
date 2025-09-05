303f2d9ce98e5406a58d2711dc426d301c0e2cd9
Niceno <bojan.niceno.scientist@gmail.com>
2025-09-05 10:15:16 +0200

    Avoids the usage of IX.  Used DE instead.
    On branch main
    
    modified:   Subs/Print_Udgs_Character_Sub.asm

--- Sources/Subs/Print_Udgs_Character_Sub.asm
+++ Sources/Subs/Print_Udgs_Character_Sub.asm
@@ -13,24 +13,24 @@
 ;-------------------------------------------------------------------------------
 Print_Udgs_Character_Sub:
 
-  ld IX, HL
+  push HL     ; store this memory address on stack (will pop as DE later)
 
   ; Calculate screen address from row and column
-  ld A, B                   ; get row number (0-23)
-  add A, A                  ; multiply by 2 (each offset is 2 bytes)
+  ld A, B                 ; get row number (0-23)
+  add A, A                ; multiply by 2 (each offset is 2 bytes)
   ld E, A
   ld D, 0
   ld HL, screen_row_offset
-  add HL, DE                ; HL points to the offset for this row
+  add HL, DE              ; HL points to the offset for this row
 
-  ; Load offset into DE
-  ld E, (HL)             ; get low byte of offset
+  ; Load offset into DE, so that you can add it to HL
+  ld E, (HL)              ; get low byte of offset
   inc HL
-  ld D, (HL)             ; get high byte of offset
+  ld D, (HL)              ; get high byte of offset
   ; Now DE = row offset
 
   ld HL, MEM_SCREEN_PIXELS
-  add HL, DE                ; HL = screen start + row offset
+  add HL, DE              ; HL = screen start + row offset
 
   ; Now add column offset
   ld A, C
@@ -38,12 +38,13 @@ Print_Udgs_Character_Sub:
   ld D, 0
   add HL, DE  ; HL now points to correct screen position
 
-  ld B, 8     ; characters are eight lines high
+  ld B, 8              ; characters are eight lines high
+  pop DE               ; this was pushed as HL with memory font start
 Print_Udgs_Character_Loop:
-  ld A, (IX)
+  ld A, (DE)
   ld(HL), A
   inc H       ; increase position at the screen (HL = HL + 256)
-  inc IX      ; increase position in the memory
+  inc DE      ; increase position in the memory
   djnz Print_Udgs_Character_Loop
 
   ret
