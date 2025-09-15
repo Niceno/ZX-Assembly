8ae6f94ae041f72328d22d9bd382f0f9be753b46
Niceno <bojan.niceno.scientist@gmail.com>
2025-09-15 18:41:30 +0200

    A VERY COOL new algorithm for attributes addresses
    This is similar to the way in which pixel addresses are computed
    and I think it is utterly cool!  It takes full advantage of Speccy's
    screen addresses and uses Boolean operations to the max.
    
    Very neat, I am loving this!!!
    
    On branch main
    
    modified:   Subs/Color_Line.asm

--- Sources/Subs/Color_Line.asm
+++ Sources/Subs/Color_Line.asm
@@ -11,6 +11,68 @@
 ;
 ; Clobbers:
 ; - AF, BC, DE, HL ... but should be double checked
+;
+; Note:
+; - The algorithm here works because:
+;
+; - Attributes start at:
+;
+;   16384 + 192 * 256 / 8 = 22528
+;   22528 = $5800 = % 01011000 00000000
+;
+;   (Note that lower eight bits (lower byte) is zero
+;
+; - Attribute rows go from 0 - 23 (24 lines),
+;   or if we analyze them third by third
+;
+;   line   binary       attribute offset
+;
+;    0  |  00000000  |  00000000
+;    1  |  00000001  |  00100000  ( 32)
+;    2  |  00000010  |  01000000  ( 64)
+;    3  |  00000011  |  01100000  ( 92)
+;    4  |  00000100  |  10000000  (128)
+;    5  |  00000101  |  10100000  (160)
+;    6  |  00000110  |  11000000  (192)
+;    7  |  00000111  |  11100000  (224)
+;
+;   (Note that these can be directly rotated to the left and then
+;    safely OR-ed with 22528, no collision here)
+;
+;   line   binary       attribute offset
+;
+;    8  |  00001000  |  01 00000000  (256)
+;    9  |  00001001  |  01 00100000  (288)
+;   10  |  00001010  |  01 01000000  (320)
+;   11  |  00001011  |  01 01100000  (352)
+;   12  |  00001100  |  01 10000000  (384)
+;   13  |  00001101  |  01 10100000  (416)
+;   14  |  00001110  |  01 11000000  (448)
+;   15  |  00001111  |  01 11100000  (480)
+;              ^         ^
+;   (Here too, lower three bits can be rotated to the left, but you also
+;    have one additional bit (position 3) which tells we are in the 2nd third
+;
+;   line   binary       attribute offset
+;
+;   16  |  00010000  |  10 00000000  (256)
+;   17  |  00010001  |  10 00100000  (288)
+;   18  |  00010010  |  10 01000000  (320)
+;   19  |  00010011  |  10 01100000  (352)
+;   20  |  00010100  |  10 10000000  (384)
+;   21  |  00010101  |  10 10100000  (416)
+;   22  |  00010110  |  10 11000000  (448)
+;   23  |  00010111  |  10 11100000  (480)
+;             ^         ^
+;   (Here too, lower three bits can be rotated to the left, but you also
+;    have one additional bit (position 4) which tells we are in the 3nd third
+;
+; - So, in order to compute the address of attributes, we can rotate the row
+;   number to the right for the lower byte and do the trick with and %00011000
+;   for the higher byte and ... things work
+;
+; - It would be great if I could also write such a lengthy explanation for the
+;   screen pixel addresses
 ;-------------------------------------------------------------------------------
 Color_Line
 
@@ -19,17 +81,25 @@ Color_Line
   ;--------------------------------------
   ; Calculate screen attributes' address
   ;--------------------------------------
+  ex AF, AF'  ; use the shadow registers for calculations
 
   ; Set proper row
   ld H, 0
   ld L, B   ; HL now holds the row number
-  add HL, HL  ; HL = HL *  2
-  add HL, HL  ; HL = HL *  4
-  add HL, HL  ; HL = HL *  8
-  add HL, HL  ; HL = HL * 16
-  add HL, HL  ; HL = HL * 32
-  ld DE, MEM_SCREEN_COLORS  ; load DE with the address of screen color
-  add HL, DE
+  sla L
+  sla L
+  sla L
+  sla L
+  sla L
+
+  ld A, B
+  and %00011000
+  rrca
+  rrca
+  rrca
+  ld   H, $58
+  or   H
+  ld   H, A
 
   ; Set the proper column
   ld D, 0
@@ -38,10 +108,12 @@ Color_Line
 
   pop DE  ; restore the column
 
+  ex AF, AF'  ; get the value of the color back
+
 .loop
     ld (HL), A
     dec E
-    inc HL  
+    inc HL
   jr nz, .loop
 
   ret
