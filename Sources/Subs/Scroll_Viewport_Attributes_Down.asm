0d199ab97ab39813dba7ce371e2d9f3e83a27884
Niceno <bojan.niceno.scientist@gmail.com>
2025-09-16 09:46:17 +0200

    Scrolling of attributes in a viewport seems fine
    On branch main
    
    modified:   012_Dawn_of_Sprites.asm
    modified:   Subs/Create_Viewport.asm
    new file:   Subs/Scroll_Viewport_Attributes_Down.asm
    new file:   Subs/Scroll_Viewport_Attributes_Up.asm

---
+++ Sources/Subs/Scroll_Viewport_Attributes_Down.asm
@@ -0,0 +1,47 @@
+;===============================================================================
+; Scroll_Viewport_Attributes_Down
+;-------------------------------------------------------------------------------
+; Purpose:
+; - Scrolls the attributes inside the viewport up
+;
+; Parameters:
+; - viewport_attribute_addresses
+;
+; Clobbers:
+; - AF, BC, DE, HL, IX
+;-------------------------------------------------------------------------------
+Scroll_Viewport_Attributes_Down
+
+  ld IX, viewport_attribute_metadata
+
+  ld C, (IX+2)  ; let the pair BC hold the number of bytes to transfer
+  ld B, 0       ; high byte is zero
+  ; At this point, the BC pair holds number of columns (to be coppied)
+
+  ; A will serve as the loop counter
+  ld A, (IX+0)  ; number of rows inside the viewport
+  dec A         ; copy one less than the dimension
+
+  ; Store number of rows in HL
+  ld L, (IX+0)
+  ld H, (IX+1)
+  ; Multiply by two, addresses are stored in two bytes
+  add HL, HL
+  ex DE, HL
+  ld IX, viewport_attribute_addresses
+  add IX, DE
+
+.loop_rows
+    push BC       ; store the number of columns
+    ld E, (IX+0)  ; first row (target) goes into DE
+    ld D, (IX+1)
+    ld L, (IX-2)  ; second row (source) goes into HL
+    ld H, (IX-1)
+    ldir          ; copy BC bytes from (HL) to (DE)
+    dec IX
+    dec IX
+    pop BC        ; restore the number of columns
+    dec A
+  jr nz, .loop_rows
+
+  ret
