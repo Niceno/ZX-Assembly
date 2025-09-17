1868d64c72805044d2ac1464bb1053e1619a2604
Niceno <bojan.niceno.scientist@gmail.com>
2025-09-17 21:12:24 +0200

    KIND OF COOL: A new version of 008_Define_Keys
    OK, first thing first, the 008_Define_Keys is a bit of a missnomer.
    It did start as a program which defines User Defined Keys (UDKs as
    I refer to them here and there), but it essentially proved to be
    much more than that.  It is, in essence, a skeleton of a Speccy
    game.
    
    Having written that, it didn't age well.  It was introduced back in
    the days when I thought that global variables (globally accessible
    addresses) were a proper way to pass arguments between the subs,
    and before the days I had the sub "Browse_Key_Rows" which browses
    the key rows and, even more importantly, creates a unique code for
    each key which was pressed.
    
    At first, I thought I cold "modernize" the 008, but it proved to be
    easier to define a new program, from the scratch, called
    008_Define_Keys_Again.asm
    
    I hope this new program will become the new skeleton for the game.
    I also hope that the old 008_Define_Keys will soon be abandoned
    and forgoten, and the new one be called something like:
    008_Game_Skeleton
    
    Let's see what future brings.
    
    On branch main
    
    new file:   008_Define_Keys_Again.asm
    modified:   compile_all.sh

---
+++ Sources/008_Define_Keys_Again.asm
@@ -0,0 +1,302 @@
+  include "Spectrum_Constants.inc"
+
+;--------------------------------------
+; Set the architecture you'll be using
+;--------------------------------------
+  device zxspectrum48
+
+;-----------------------------------------------
+; Memory address at which the program will load
+;-----------------------------------------------
+  org MEM_PROGRAM_START
+
+;%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+;
+;   MAIN PROGRAM
+;
+;%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+;===============================================================================
+; Main subroutine begins here
+;-------------------------------------------------------------------------------
+Main:
+
+;%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+; SECTION 1/X: DEFINE KEYS
+;%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+  ;--------------------------
+  ;
+  ;
+  ; Loop to define five keys
+  ;
+  ;
+  ;--------------------------
+  ld C, 0  ; need 5 keys
+
+.define_five_keys:
+
+    ;------------------------
+    ;
+    ; Print a prompting text
+    ;
+    ;------------------------
+    push BC  ; keep the counter safe
+
+    ld   A, C
+    ld   L, A
+    ld   H, 0
+    add  HL, HL          ; index * 2 (word table)
+    ld   DE, text_table
+    add  HL, DE          ; HL -> defw entry
+
+    ld   E, (HL)  ; load string ptr
+    inc  HL
+    ld   D, (HL)
+    ex   DE, HL   ; HL = prompt string
+
+    ld   A, C          ; compute the row ...
+    add  A, A          ; ... as twice the counter
+    ld   B, A          ; set row
+    ld   C, 1          ; set column
+    call Print_String  ; prints zero-terminated string at HL
+
+    pop  BC  ; restore the counter
+
+    ;-----------------------------
+    ;
+    ; Browse through all key rows
+    ;
+    ;-----------------------------
+    push BC               ; save the counter in C (through five keys)
+    call Browse_Key_Rows  ; A = unique code, C bit0 = 1 if any key pressed
+    bit  0, C             ; check C register's zeroth bit
+    pop BC                ; retreive the counter in C
+
+    jr z, .define_five_keys   ; no key pressed -> keep polling
+
+    ;---------------------------------------------------
+    ;
+    ; A key was pressed - wait untill it gets unpressed
+    ;
+    ;---------------------------------------------------
+    push AF       ; keep unique code in A safe
+    push BC       ; save the counter in C
+    call Unpress  ; wait until all keys released
+    pop BC        ; restore the counter in C
+    pop AF        ; restore the unique code in A
+
+    ;---------------------------
+    ;
+    ; Process the key in A here
+    ;
+    ;---------------------------
+
+    ; Store the defined key ... very important!!!
+    ld HL, five_defined_keys
+    ld B, 0     ; with B set to zero, the pair BC will be the offset
+    add HL, BC  ; add the offset to five defined keys
+    ld (HL), A  ; store the key's unique code
+
+    ; Print the key you just pressed ... quite handy
+    ld HL, all_characters_mem_coded  ; point to all key characters table
+    ld D, 0                          ; create offset from unique key code ...
+    ld E, A                          ; ... (stored in A) in the the DE pair
+    add HL, DE                       ; add the offset ...
+    add HL, DE                       ; ... to HL pair
+    ld E, (HL)                       ; Load DE pair with ...
+    inc HL                           ; ... the address to which ...
+    ld D, (HL)                       ; ... HL correclty point
+    ex DE, HL                        ; ... and put it back to HL!!!
+
+    push BC                    ; save key counter (in C)
+    ld   A, C                  ; compute the row ...
+    add  A, A                  ; ... as twice the counter
+    ld   B, A                  ; set row
+    ld   C, 22                 ; set column
+    call Print_Udgs_Character
+    pop BC                     ; restore the counter
+
+    ; Check if counter reached five
+    inc C
+    ld A, C
+    cp 5
+
+  jp nz, .define_five_keys          ; loop until we've taken 5 presses
+
+;%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+; SECTION 2/X: PLAY THE GAME
+;%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+  ;-------------------------
+  ;
+  ;
+  ; Prepare the main screen
+  ;
+  ;
+  ;-------------------------
+
+  ;------------------
+  ; Clear the screen
+  ;------------------
+  call ROM_CLEAR_SCREEN           ; clear the screen
+
+  ;----------------
+  ;
+  ;
+  ; Main game loop
+  ;
+  ;
+  ;----------------
+.main_game_loop:
+
+    ;-----------------------------
+    ;
+    ; Browse through all key rows
+    ;
+    ;-----------------------------
+    call Browse_Key_Rows  ; A = unique code, C bit0 = 1 if any key pressed
+    bit  0, C             ; check C register's zeroth bit
+
+    jr z, .main_game_loop  ; no key pressed -> keep polling
+
+    ;---------------------------------------------
+    ;
+    ; A key was pressed - process the action here
+    ; (Note that here A holds the unique key code)
+    ;
+    ;---------------------------------------------
+
+    ;---------------
+    ; Action for up
+    ;---------------
+.was_the_key_for_up_pressed:
+    ld HL, key_for_up
+    cp (HL)
+    jr nz, .was_the_key_for_down_pressed
+
+    ld HL, arrow_up
+    ld B, 1 : ld C, 1
+    call Print_Udgs_Character
+
+    jr .main_game_loop
+
+    ;-----------------
+    ; Action for down
+    ;-----------------
+.was_the_key_for_down_pressed:
+    ld HL, key_for_down
+    cp (HL)
+    jr nz, .was_the_key_for_left_pressed
+
+    ld HL, arrow_down
+    ld B, 1 : ld C, 1
+    call Print_Udgs_Character
+
+    jr .main_game_loop
+
+    ;-----------------
+    ; Action for left
+    ;-----------------
+.was_the_key_for_left_pressed:
+    ld HL, key_for_left
+    cp (HL)
+    jr nz, .was_the_key_for_right_pressed
+
+    ld HL, arrow_left
+    ld B, 1 : ld C, 1
+    call Print_Udgs_Character
+
+    jr .main_game_loop
+
+    ;------------------
+    ; Action for right
+    ;------------------
+.was_the_key_for_right_pressed:
+    ld HL, key_for_right
+    cp (HL)
+    jr nz, .was_the_key_for_fire_pressed
+
+    ld HL, arrow_right
+    ld B, 1 : ld C, 1
+    call Print_Udgs_Character
+
+    jr .main_game_loop
+
+.was_the_key_for_fire_pressed:
+    ld HL, key_for_fire
+    cp (HL)
+    jr nz, .were_all_keys_pressed
+
+    ld HL, fire
+    ld B, 1 : ld C, 1
+    call Print_Udgs_Character
+
+    jr .main_game_loop
+
+.were_all_keys_pressed:
+
+;%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+; SECTION 3/X: GAME OVER
+;%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+  ei   ; <--= (re)enable interrupts if you want to return to OS/BASIC
+
+  ret  ; end of the main program
+
+;%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+;
+;   SUBROUTINES
+;
+;%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  include "Subs/Open_Upper_Screen.asm"
+  include "Subs/Calculate_Screen_Attribute_Address.asm"
+  include "Subs/Color_Line.asm"
+  include "Subs/Press_Any_Key.asm"
+  include "Subs/Unpress.asm"
+  include "Subs/Calculate_Screen_Pixel_Address.asm"
+  include "Subs/Print_Character.asm"
+  include "Subs/Print_String.asm"
+  include "Subs/Udgs/Print_Character.asm"
+  include "Subs/Delay.asm"
+  include "Subs/Print_08_Bit_Number.asm"
+  include "Subs/Browse_Key_Rows.asm"
+
+;%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+;
+;   DATA
+;
+;%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  include "Global_Data.inc"
+  include "Unique_Key_Codes_Sorted_By_Values.inc"
+
+;-------------------------------
+; Storage for user defined keys
+;-------------------------------
+five_defined_keys:
+key_for_up:     defb  0
+key_for_down:   defb  0
+key_for_left:   defb  0
+key_for_right:  defb  0
+key_for_fire:   defb  0
+
+arrow_up:      defb $00, $18, $3C, $7E, $18, $18, $18, $00
+arrow_down:    defb $00, $18, $18, $18, $7E, $3C, $18, $00
+arrow_left:    defb $00, $10, $30, $7E, $7E, $30, $10, $00
+arrow_right:   defb $00, $08, $0C, $7E, $7E, $0C, $08, $00
+fire:          defb $08, $04, $0C, $2A, $3A, $7A, $66, $3C
+
+text_table:
+  defw text_for_up, text_for_down, text_for_left, text_for_right, text_for_fire
+
+text_for_up:    defb "Press key for UP    [ ]", 0
+text_for_down:  defb "Press key for DOWN  [ ]", 0
+text_for_left:  defb "Press key for LEFT  [ ]", 0
+text_for_right: defb "Press key for RIGHT [ ]", 0
+text_for_fire:  defb "Press key for FIRE  [ ]", 0
+
+;-------------------------------------------------------------------------------
+; Save a snapshot that starts execution at the address marked with Main
+;-------------------------------------------------------------------------------
+  savesna "bojan_008.sna", Main
+  savebin "bojan_008.bin", Main, $ - Main
